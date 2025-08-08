4529a3e896e1824a24892febdb6fd54c2171f529
Souravp-94 <sourav1994padhi@gmail.com>
2025-08-09 04:56:11 +0530

    sencondLongest

--- src/test/java/String_Collections_Map/newArrGivenSum.java
+++ src/test/java/String_Collections_Map/newArrGivenSum.java
@@ -1,25 +1,29 @@
 package String_Collections_Map;
 
-import java.util.ArrayList;
+import java.util.*;
+
 public class newArrGivenSum {
         public static void main(String[] args) {
-            int[] arr = {10, 6, 89, 97, 1, 3};
-            int targetSum = 101;
-            ArrayList<Integer> result = findNumbersWithSum(arr, targetSum);
+            int[] arr = {10, 6, 89, 97, 13, 3};
+            int targetSum = 16;
+            Set<ArrayList<Integer>> result = findNumbersWithSum(arr, targetSum);
 
             System.out.println("Numbers that sum up to " + targetSum + ": " + result);
         }
-        public static ArrayList<Integer> findNumbersWithSum(int[] arr, int targetSum) {
-            ArrayList<Integer> result = new ArrayList<>();
-            for (int i = 0; i < arr.length; i++) {
-                for (int j = i + 1; j < arr.length; j++) {
-                    if (arr[i] + arr[j] == targetSum) {
-                        result.add(arr[i]);
-                        result.add(arr[j]);
+        public static Set<ArrayList<Integer>> findNumbersWithSum(int[] arr, int targetSum) {
+            Set<ArrayList<Integer>> n = new LinkedHashSet<>();
+            for(int i = 0 ; i <= arr.length -1 ; i++){
+                for(int j = 1 ; j <= arr.length -1 ; j++){
+                    if(arr[i] + arr[j] == targetSum){
+                        ArrayList<Integer> list = new ArrayList<>();
+                        list.add(arr[i]);
+                        list.add(arr[j]);
+                        Collections.sort(list);
+                        n.add(list);
                     }
                 }
             }
-            return result;
+            return n;
         }
     }
 
